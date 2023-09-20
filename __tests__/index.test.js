39e1ffbdb8f214b5490f341b5601eb8db7de5f95
nikitusyandra <chexov04@inbox.ru>
2023-09-20 11:08:28 +0300

    ghgyjy

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,12 @@
-import gendiff from '../src/index.js'
-import result from '../__fixtures__/result.js'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import gendiff from '../src/index.js';
+import result from '../__fixtures__/result.js';
 
-test('json',() =>{
-    expect(gendiff('/__fixtures__/file1.json', '/__fixtures__/file2.json')).toBe(result);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('json', () => {
+  expect(gendiff('/__fixtures__/file1.json', '/__fixtures__/file2.json')).toBe(result);
 });
