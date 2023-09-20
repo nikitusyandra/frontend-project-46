5cb30f2c968160e06e6e06f3bd3a10cf7036a83a
nikitusyandra <chexov04@inbox.ru>
2023-09-20 14:32:43 +0300

    commitYAML

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,3 +4,11 @@ import result from '../__fixtures__/result.js';
 test('json', () => {
   expect(gendiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(result);
 });
+
+test('yml', () => {
+  expect(gendiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml')).toBe(result);
+});
+
+test('yaml', () => {
+  expect(gendiff('./__fixtures__/file1.yaml', './__fixtures__/file2.yaml')).toBe(result);
+});
---
+++ src/parsers.js
@@ -0,0 +1,10 @@
+import { readFileSync } from 'fs';
+import path from 'node:path';
+import yaml from 'js-yaml';
+
+
+const parser = (filename) => (path.basename(filename) !== '.json'
+  ? yaml.load(readFileSync(filename, 'utf-8'))
+  : JSON.parse(readFileSync(filename, 'utf-8')));
+
+export default parser;
